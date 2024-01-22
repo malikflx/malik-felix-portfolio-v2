1a0423b8ebaf59588f5235a372018aec804cb0f2
Malik <malikfelix50@gmail.com>
2024-01-22 08:20:21 -0600

    add pure-vibez card to work section

--- src/Layouts/Work/Work.jsx
+++ src/Layouts/Work/Work.jsx
@@ -1,41 +1,23 @@
 import { Link } from "react-router-dom";
 import PureVibez from "../../assets/images/pure-vibez-screen.png";
 import "./Work.css";
+import SecondaryButton from "../../Components/Buttons/SecondaryButton";
 
 const Work = () => {
   return (
     <div className="work">
       <h2 className="work-title">Work</h2>
       <div className="work-container">
-        <figure className="fig-item"></figure>
-        {/* <Link to="https://purevibez.netlify.app">
-          <div className="work-item">
-            <div className="image-section">
-              <img src={PureVibez} alt="pure-vibez-logo" />
-            </div>
+        <div className="image">
+          <img src={PureVibez} alt="pure-vibez-logo" />
+          <div className="content">
+            <h3>Pure Vibez</h3>
+            <p>Website for bartending & catering service</p>
+            <SecondaryButton className="work-button pv-button">
+              Visit Website
+            </SecondaryButton>
           </div>
-        </Link>
-        <Link to="https://purevibez.netlify.app">
-          <div className="work-item">
-            <div className="image-section">
-              <img src={PureVibez} alt="pure-vibez-logo" />
-            </div>
-          </div>
-        </Link>
-        <Link to="https://purevibez.netlify.app">
-          <div className="work-item">
-            <div className="image-section">
-              <img src={PureVibez} alt="pure-vibez-logo" />
-            </div>
-          </div>
-        </Link>
-        <Link to="https://purevibez.netlify.app">
-          <div className="work-item">
-            <div className="image-section">
-              <img src={PureVibez} alt="pure-vibez-logo" />
-            </div>
-          </div>
-        </Link> */}
+        </div>
       </div>
     </div>
   );
