71429e6144a79f6b82b6ed2d75614136b5d5ea22
Malik <malikfelix50@gmail.com>
2024-01-21 20:56:00 -0600

    add navbar css

--- src/Layouts/Work/Work.jsx
+++ src/Layouts/Work/Work.jsx
@@ -1,9 +1,42 @@
+import { Link } from "react-router-dom";
+import PureVibez from "../../assets/images/pure-vibez-screen.png";
 import "./Work.css";
 
 const Work = () => {
   return (
     <div className="work">
-      <h2>Work</h2>
+      <h2 className="work-title">Work</h2>
+      <div className="work-container">
+        <figure className="fig-item"></figure>
+        {/* <Link to="https://purevibez.netlify.app">
+          <div className="work-item">
+            <div className="image-section">
+              <img src={PureVibez} alt="pure-vibez-logo" />
+            </div>
+          </div>
+        </Link>
+        <Link to="https://purevibez.netlify.app">
+          <div className="work-item">
+            <div className="image-section">
+              <img src={PureVibez} alt="pure-vibez-logo" />
+            </div>
+          </div>
+        </Link>
+        <Link to="https://purevibez.netlify.app">
+          <div className="work-item">
+            <div className="image-section">
+              <img src={PureVibez} alt="pure-vibez-logo" />
+            </div>
+          </div>
+        </Link>
+        <Link to="https://purevibez.netlify.app">
+          <div className="work-item">
+            <div className="image-section">
+              <img src={PureVibez} alt="pure-vibez-logo" />
+            </div>
+          </div>
+        </Link> */}
+      </div>
     </div>
   );
 };
