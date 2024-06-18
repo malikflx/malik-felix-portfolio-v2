64a87c87e24538d87494f6853eed68cf1f5a34ee
Malik <malikfelix50@gmail.com>
2024-06-18 18:01:19 -0500

    update work section mobile styles

--- src/Layouts/Work/Work.jsx
+++ src/Layouts/Work/Work.jsx
@@ -3,35 +3,49 @@ import PureVibez from "../../assets/images/pure-vibez-screen.png";
 import AwdcLanding from "../../assets/images/awdc-logo-lower-spaced-no-background.png";
 import "./Work.css";
 import SecondaryButton from "../../Components/Buttons/SecondaryButton";
-import SectionBorder from "../../Components/Dividers/SectionBorder";
 
 const Work = () => {
   return (
     <div className="work">
       <h2 className="work-title">Work</h2>
-      <SectionBorder />
       <div className="work-container">
-        <div className="work-item pv-image">
-          <img src={PureVibez} alt="pure-vibez-logo" />
-          <div className="content pv-content">
-            <h3>Pure Vibez</h3>
-            <p>Website for a bartending & catering service</p>
+        <div className="selected-work">
+          <div className="work-item pv-image">
+            <img src={PureVibez} alt="pure-vibez-logo" />
+            <div className="content pv-content">
+              <h3>Pure Vibez</h3>
+              <p>Website for a bartending & catering service</p>
+              <Link to="https://purevibez.netlify.app/">
+                <SecondaryButton className="work-button">
+                  Visit Website
+                </SecondaryButton>
+              </Link>
+            </div>
+          </div>
+          <div className="work-subheading">
             <Link to="https://purevibez.netlify.app/">
-              <SecondaryButton className="work-button">
-                Visit Website
-              </SecondaryButton>
+              <h3>Pure Vibez</h3>
+              <p>Website | 1st Iteration</p>
             </Link>
           </div>
         </div>
-        <div className="work-item awdc-image">
-          <img src={AwdcLanding} alt="awdc-landing-page-thumbnail" />
-          <div className="content awdc-content">
-            <h3>A Web Development Company (Landing Page)</h3>
-            <p>Landing Page for an upcoming agency</p>
+        <div className="selected-work">
+          <div className="work-item awdc-image">
+            <img src={AwdcLanding} alt="awdc-landing-page-thumbnail" />
+            <div className="content awdc-content">
+              <h3>A Web Development Company (Landing Page)</h3>
+              <p>Landing Page for an upcoming agency</p>
+              <Link to="https://awdc.netlify.app/">
+                <SecondaryButton className="work-button">
+                  Visit Website
+                </SecondaryButton>
+              </Link>
+            </div>
+          </div>
+          <div className="work-subheading">
             <Link to="https://awdc.netlify.app/">
-              <SecondaryButton className="work-button">
-                Visit Website
-              </SecondaryButton>
+              <h3>A Web Development Company</h3>
+              <p>Website | Landing Page</p>
             </Link>
           </div>
         </div>
